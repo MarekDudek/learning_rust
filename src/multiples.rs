82fccd4c2e570352c5ff1d167d1a5f09ad69ccdb
Marek Dudek <marek.dudek@gmail.com>
2016-10-26 23:51:56 +0200

    first solved in non-functional way

--- src/multiples.rs
+++ src/multiples.rs
@@ -1,7 +1,13 @@
 
 #[allow(dead_code)]
 pub fn multiples() -> i64 {
-    23
+    let range = 0..10;
+    let divisible = range.filter( |x| x % 3 == 0 || x % 5 == 0);
+    let mut sum = 0;
+    for x in divisible {
+        sum = sum + x;
+    }
+    sum 
 }
 
 
