fc6db8ebbc2ce134f13b999c07e960c947c87b1f
Marek Dudek <marek.dudek@gmail.com>
2016-10-27 00:09:30 +0200

    functional solution with fold

--- src/multiples.rs
+++ src/multiples.rs
@@ -10,6 +10,11 @@ pub fn multiples(upper_bound: i64) -> i64 {
     sum
 }
 
+#[allow(dead_code)]
+pub fn multiples_fun(upper_bound: i64) -> i64 {
+    let divisible = (0..upper_bound).filter(|x| x % 3 == 0 || x % 5 == 0);
+    divisible.fold(0, |sum, x| sum + x)
+}
 
 #[cfg(test)]
 mod tests {
@@ -24,5 +29,15 @@ mod tests {
     #[test]
     fn solution() {
         assert_eq!(multiples(1000), 233168);
+    }
+
+    #[test]
+    fn example_fun() {
+        assert_eq!(multiples_fun(10), 23);
+    }
+
+    #[test]
+    fn solution_fun() {
+        assert_eq!(multiples_fun(1000), 233168);
     }
 }
