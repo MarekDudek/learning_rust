d78595ad9657de79d8af078c0fbf142349f69107
Marek Dudek <marek.dudek@gmail.com>
2016-10-27 20:42:29 +0200

    clarified

--- src/even_fibonacci_numbers.rs
+++ src/even_fibonacci_numbers.rs
@@ -1,11 +1,11 @@
 
 
 #[allow(dead_code)]
-pub fn fibonacci(i: u64) -> u64  {
+pub fn fibonacci(i: u64) -> u64 {
     match i {
         0 => 0,
         1 => 1,
-        n => fibonacci(n-2) + fibonacci(n-1)
+        n => fibonacci(n - 2) + fibonacci(n - 1),
     }
 }
 
@@ -13,21 +13,24 @@ pub fn fibonacci(i: u64) -> u64  {
 pub fn sum_of_even_fibonacci_up_to(n: u64) -> u64 {
     let mut sum = 0;
     let mut i = 0;
-    let mut fib: u64 = 0;
+    let mut fib = 0;
     while fib <= n {
         if fib % 2 == 0 {
-            sum = sum+fib;
+            sum = sum + fib;
         }
         fib = fibonacci(i);
-        i = i+1;
+        i = i + 1;
     }
     sum
 }
 
 #[allow(dead_code)]
-pub fn sum_of_even_fibonacci_up_to_fun(n: u64) -> u64 {
-    let series = (0..).map(|i| fibonacci(i)).filter(|f| f % 2 == 0).take_while(|f| f <= &n);
-    series.fold(0, |sum, n| sum + n)
+pub fn sum_of_even_fibonacci_up_to_fun(threshold: u64) -> u64 {
+    let integrals = 0..;
+    let fibonacci = integrals.map(|i| fibonacci(i));
+    let even = fibonacci.filter(|f| f % 2 == 0);
+    let up_to = even.take_while(|f| f <= &threshold);
+    up_to.fold(0, |sum, n| sum + n)
 }
 
 
@@ -58,12 +61,11 @@ mod tests {
 
     #[test]
     fn sum_up_to_four_million() {
-        assert_eq!(sum_of_even_fibonacci_up_to(4000000),4613732 );
+        assert_eq!(sum_of_even_fibonacci_up_to(4000000), 4613732);
     }
 
     #[test]
     fn sum_up_to_four_million_fun() {
-        assert_eq!(sum_of_even_fibonacci_up_to_fun(4000000),4613732 );
+        assert_eq!(sum_of_even_fibonacci_up_to_fun(4000000), 4613732);
     }
 }
-
