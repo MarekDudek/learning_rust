e6208f55972a66c5982b697865a12071d0da7e58
Marek Dudek <marek.dudek@gmail.com>
2016-10-30 20:14:11 +0100

    started mind-map

---
+++ docs/Rust.mm
@@ -0,0 +1,51 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1477853622963" ID="ID_519155126" MODIFIED="1477853664743" TEXT="Rust">
+<node CREATED="1477853680786" ID="ID_750260429" MODIFIED="1477854420774" POSITION="right" TEXT="Variable Bindings">
+<node CREATED="1477853691139" ID="ID_908846492" MODIFIED="1477853699142" TEXT="binds value to a name">
+<node CREATED="1477853700628" ID="ID_1049832575" MODIFIED="1477853737432" TEXT="to be used later"/>
+</node>
+<node CREATED="1477853758276" ID="ID_790011824" MODIFIED="1477854254667">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      left-hand side of <b>let</b>&#160;statement is a <i>pattern</i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1477854071197" ID="ID_1885701567" MODIFIED="1477854136571" TEXT="required to be initialized before using"/>
+<node CREATED="1477854140689" ID="ID_873609598" MODIFIED="1477854209240" TEXT="have a scope">
+<node CREATED="1477854209998" ID="ID_1026854895" MODIFIED="1477854211404" TEXT="constrained to block they were defined in"/>
+<node CREATED="1477854202171" ID="ID_22603149" MODIFIED="1477854202906" TEXT="can be shadowed "/>
+</node>
+<node CREATED="1477853880777" ID="ID_1089968336" MODIFIED="1477853885844" TEXT="immutable by default">
+<node CREATED="1477853962932" ID="ID_639036075" MODIFIED="1477853993193">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>mut</b>&#160;keyword
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1477854257866" ID="ID_168572418" MODIFIED="1477854364097" TEXT="shadowing is different from mutability">
+<node CREATED="1477854287058" ID="ID_334105000" MODIFIED="1477854297597" TEXT="shadowing enables to rebind a name"/>
+<node CREATED="1477854321026" ID="ID_1285276796" MODIFIED="1477854374519" TEXT="does not alter or destroy the value it was bound to"/>
+<node CREATED="1477854336080" ID="ID_1612951198" MODIFIED="1477854348722" TEXT="value will continue to exist until it goes out of scope"/>
+<node CREATED="1477854384797" ID="ID_1467461211" MODIFIED="1477854401086" TEXT="possibly no longer accessible"/>
+</node>
+</node>
+<node CREATED="1477854423453" ID="ID_1273206605" MODIFIED="1477854425382" POSITION="left" TEXT="Functions"/>
+</node>
+</map>
